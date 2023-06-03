b696fdabf94a38dcd5ab76b94ed5271b31775702
Jose <jsut914@gmail.com>
2023-06-03 16:05:32 -0600

    Finalizando clase 02

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Encabezado from './componentes/header/header.js';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Encabezado/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendiendo sobre React
-        </a>
-      </header>
     </div>
   );
 }
