e884655e7773fe3a7629a3efba155c1d28061c39
Jose <jsut914@gmail.com>
2023-06-03 17:49:10 -0600

    Clase 03 inicio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Encabezado from './componentes/header/header.js';
+import Encabezado from './componentes/header/header';
+import Formulario from './componentes/formulario/formulario';
 
 function App() {
   return (
     <div>
       <Encabezado/>
+      <Formulario/>
     </div>
   );
 }
---
+++ src/componentes/formulario/formulario.js
@@ -0,0 +1,11 @@
+import "./formulario.css";
+
+const Formulario = () =>{
+    return <section className="formulario">
+        <form>
+            <h2>Rellena el formulario para tener el colaborador.</h2>
+        </form>
+    </section>
+}
+
+export default Formulario; 
