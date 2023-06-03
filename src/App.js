867ab5992cb775f48794c1ffff27110b4e9ebfa7
Jose <jsut914@gmail.com>
2023-06-03 12:55:49 -0600

    clase 02 inicio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Encabezado from './componentes/header/header.js';
 
 function App() {
   return (
     <div className="App">
+      <Encabezado/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/componentes/header/header.js
@@ -0,0 +1,9 @@
+import "./header.css"; 
+
+function Encabezado(){
+    return <header className="header">
+        <img src="/imagenes/encabezado.png" alt="Org"/>
+    </header>
+}
+
+export default Encabezado; 
