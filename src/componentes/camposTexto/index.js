a5c644266960761d7962b20d2051a1f56130daca
Jose <jsut914@gmail.com>
2023-06-03 18:27:29 -0600

    Clase 03 estilización de inputs

---
+++ src/componentes/camposTexto/index.js
@@ -0,0 +1,10 @@
+import "./campos-texto.css"; 
+
+const CamposTexto = () =>{
+    return <div className="campo-texto">
+        <label>Nombre</label>
+        <input placeholder="Ingrese su nombre: "/>
+    </div>
+}
+
+export default CamposTexto; 
--- src/componentes/formulario/formulario.js
+++ src/componentes/formulario/formulario.js
@@ -1,9 +1,11 @@
 import "./formulario.css";
+import CamposTexto from "../camposTexto";
 
 const Formulario = () =>{
     return <section className="formulario">
         <form>
             <h2>Rellena el formulario para tener el colaborador.</h2>
+            <CamposTexto/>
         </form>
     </section>
 }
