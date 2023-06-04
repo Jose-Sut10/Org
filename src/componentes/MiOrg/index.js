8f1bdf721bbf2fe38d9f1117401049c8ba21cdfd
Jose <jsut914@gmail.com>
2023-06-04 09:14:41 -0600

    Clase 05 inicio

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./MiOrg.css";
 
 const MiOrg= () =>{
+    //Estado -> hookd
+    //usesState
+    //usesState()
+    //const [variable,funcionActualizadora] = useState(valorInicial);
+    const [dato, datoActualizado] = useState(true); 
+
+    const clickImagen = ()=>{
+        console.log("Si funciona", !datoActualizado); 
+        datoActualizado(!dato); 
+    }
+
     return <section className="orgSection">
         <h3 className="tituloOrg">Mi Organización</h3>
-        <img src="/imagenes/add.png" alt="Icono"/>
+        <img src="/imagenes/add.png" alt="Icono" onClick={clickImagen}/>
     </section> 
 }
 
