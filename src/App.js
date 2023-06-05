b95879b9e8bf84b02919f8710078370c020c315e
Jose <jsut914@gmail.com>
2023-06-04 20:16:36 -0600

    Clase 05 finalización

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Encabezado from './componentes/header/header';
 import Formulario from './componentes/formulario/formulario';
 import MiOrg from './componentes/MiOrg';
 
 function App() {
+  const [mostrarFormulario, actualizarFormulario] = useState(true);
+  const cambioMostrar = ()=>{
+    actualizarFormulario(!mostrarFormulario);
+  }
+
+  //Ternario --> condicion ? semuestra : noseMuestra
   return (
     <div>
       <Encabezado/>
-      <Formulario/>
-      <MiOrg />
+      {mostrarFormulario && <Formulario/>}
+      <MiOrg estado={cambioMostrar}/>
     </div>
   );
 }
--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,21 +1,15 @@
-import { useState } from "react";
 import "./MiOrg.css";
 
-const MiOrg= () =>{
-    //Estado -> hookd
-    //usesState
+const MiOrg= (props) =>{
+    //Estado -> hooks
     //usesState()
     //const [variable,funcionActualizadora] = useState(valorInicial);
-    const [dato, datoActualizado] = useState(true); 
+    // const [dato, datoActualizado] = useState(true); 
 
-    const clickImagen = ()=>{
-        console.log("Si funciona", !datoActualizado); 
-        datoActualizado(!dato); 
-    }
 
     return <section className="orgSection">
         <h3 className="tituloOrg">Mi Organización</h3>
-        <img src="/imagenes/add.png" alt="Icono" onClick={clickImagen}/>
+        <img src="/imagenes/add.png" alt="Icono" onClick={props.estado}/>
     </section> 
 }
 
