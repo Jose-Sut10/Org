511438badc2348332ec92b336a9c89846e231880
Jose <jsut914@gmail.com>
2023-06-06 00:11:07 -0600

    Clase 06 inicio

--- src/componentes/camposTexto/index.js
+++ src/componentes/camposTexto/index.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./campos-texto.css"; 
 
 const CamposTexto = (props) =>{
     const textoDescripcion = `${props.descripcion}...`;
+
+    const valorInput = (event) =>{
+        props.actualizarValor(event.target.value); 
+    }
+
     return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder={textoDescripcion} required={props.importancia}/>
+        <input 
+            placeholder={textoDescripcion} 
+            required={props.importancia} 
+            value={props.valor}
+            onChange={valorInput}
+        />
     </div>
 }
 
--- src/componentes/formulario/formulario.js
+++ src/componentes/formulario/formulario.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./formulario.css";
 import CamposTexto from "../camposTexto";
 import ListaOpciones from "../lista-opciones";
@@ -5,16 +6,48 @@ import BotonCrear from "../boton";
 
 const Formulario = () =>{
 
+    const [nombre,actualizarNombre] = useState("");
+    const [puesto,actualizarPuesto] = useState(""); 
+    const [foto,actualizarFoto] = useState(""); 
+
     const eventoEnviar = (event)=>{
         event.preventDefault(); 
+
+        let datosEnviar = {
+            nombreColab: nombre,
+            puestoColab: puesto, 
+            fotoColab: foto
+        }
+        console.log(datosEnviar); 
     }
 
     return <section className="formulario">
         <form onSubmit={eventoEnviar}>
             <h2>Rellena el formulario para tener el colaborador.</h2>
-            <CamposTexto titulo="Nombre" descripcion="Ingresar nombre: " importancia/>
-            <CamposTexto titulo="Puesto" descripcion="Ingresar puesto: " importancia/>
-            <CamposTexto titulo="Foto" descripcion="Ingresar URL de la foto: " importancia/>
+            <CamposTexto 
+                titulo="Nombre" 
+                descripcion="Ingresar nombre: " 
+                importancia
+                valor={nombre}
+                actualizarValor={actualizarNombre}
+            />
+
+            <CamposTexto 
+                titulo="Puesto" 
+                descripcion="Ingresar puesto: " 
+                importancia
+                valor={puesto}
+                actualizarValor={actualizarPuesto}
+            />
+
+            <CamposTexto 
+                titulo="Foto" 
+                descripcion="Ingresar URL de la foto: " 
+                importancia
+                valor={foto}
+                actualizarValor={actualizarFoto}
+            />
+
             <ListaOpciones />
             <BotonCrear>
                 Crear
